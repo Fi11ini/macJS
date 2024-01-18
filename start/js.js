259fa6fef28ce021bd7e451089cb8ee2d8bbf642
Andrey <andreymdm@gmail.com>
2024-01-18 22:23:30 +0300

    task 5

--- start/js.js
+++ start/js.js
@@ -1,35 +1,20 @@
 "use strict";
 
-const arr = [
-  "John",
-  "Travolta",
-  1,
-  "Moscow",
-  (3)[("Anna", "Peter", "Ivan")],
-  7,
-];
 
-// const arr2 = []
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
 
-// for(let i = 0; i < arr.length; i++) {
-//     console.log(typeof arr[i])
-//     arr2.push(typeof arr[i])
-// }
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
 
-// console.log(arr2)
+  else {
+    result = "5 or Smaller";
+  }
 
-// for (let i = 0; i < arr.length; i++) {
-//   if (typeof arr[i] !== "string") continue;
-//   console.log(arr[i]);
-// } 
-
-
-let someNumber = Math.floor(Math.random() * 10)
-console.log(someNumber)
-
-while(someNumber !== 10) {
-    console.log(`Вам выпало число ${someNumber}`)
-    someNumber = Math.floor(Math.random() * 10) + 1
-} if(someNumber ===10) {
-    console.log('Вы победили, вам выпало число 10')
+  // Only change code above this line
+  return result;
 }
+
+testElse(4);
